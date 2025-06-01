8c8c00c540fe8b988d1d04f87cf97c87410c4b26
HuXin0817 <202219120810@stu.cdut.edu.cn>
2025-06-01 22:42:19 +0800

    perf(utils): Parallelize MSE calculations using OpenMP to improve performance
    Add OpenMP parallel instructions in the compute_mse function to speed up the calculation process of mean square error (MSE) through multi-threaded parallel computing.

--- libpouq/utils.h
+++ libpouq/utils.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <omp.h>
+
 template <typename D1, typename D2, typename T>
 float compute_mse(const D1 &d1, const D2 &d2, T size) {
   float mse = 0;
+#pragma omp parallel for reduction(+ : mse)
   for (T i = 0; i < size; ++i) {
     const float dif = d1[i] - d2[i];
     mse += dif * dif;
