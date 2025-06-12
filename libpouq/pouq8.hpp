6317e048635490c0346f8d4cf19d32e81b9ad0c7
HuXin0817 <202219120810@stu.cdut.edu.cn>
2025-06-13 02:23:31 +0800

    feat: Add SIMD optimization implementation of ARM NEON architecture
    Added vectorized calculation support for ARM NEON and aarch64 architectures, and optimized L2 distance calculation performance using the NEON instruction set

--- libpouq/pouq8.hpp
+++ libpouq/pouq8.hpp
@@ -7,6 +7,8 @@
 
 #if defined(__i386__) || defined(__x86_64__)
 #include <immintrin.h>
+#elif defined(__ARM_NEON) || defined(__aarch64__)
+#include <arm_neon.h>
 #endif
 
 namespace pouq {
@@ -116,6 +118,63 @@ public:
     sum_128  = _mm_hadd_ps(sum_128, sum_128);
     sum_128  = _mm_hadd_ps(sum_128, sum_128);
     distance = _mm_cvtss_f32(sum_128);
+
+    for (size_t i = simd_end; i < dimension_; i++) {
+      uint8_t encoded_value     = encoded_codes_[data_index + i];
+      auto [lower_bound, scale] = codebook_[((encoded_value & 0xF) + i * (1 << 4))];
+      float decoded_value       = lower_bound + scale * (encoded_value >> 4 & 0xF);
+      float diff                = data[i] - decoded_value;
+      distance += diff * diff;
+    }
+
+    return distance;
+  }
+#elif defined(__ARM_NEON) || defined(__aarch64__)
+  float l2distance(const float *data, size_t data_index) const {
+    float distance = 0.0f;
+
+    const size_t simd_end   = (dimension_ / 4) * 4;
+    float32x4_t  sum_vector = vdupq_n_f32(0.0f);
+
+    for (size_t i = 0; i < simd_end; i += 4) {
+      float32x4_t data_vector = vld1q_f32(&data[i]);
+
+      float32x4_t decoded_vector;
+      {
+        alignas(16) float   decoded_values[4];
+        alignas(16) uint8_t code_batch[4];
+
+        for (size_t j = 0; j < 4; j++) {
+          code_batch[j] = encoded_codes_[data_index + i + j];
+        }
+
+        uint32x4_t code_vector   = vmovl_u8(vld1_u8(code_batch));
+        uint32x4_t lower_nibbles = vandq_u32(code_vector, vdupq_n_u32(0xF));
+        uint32x4_t upper_nibbles = vandq_u32(vshrq_n_u32(code_vector, 4), vdupq_n_u32(0xF));
+
+        alignas(16) uint32_t lower_indices[4];
+        alignas(16) uint32_t upper_values[4];
+        vst1q_u32(lower_indices, lower_nibbles);
+        vst1q_u32(upper_values, upper_nibbles);
+
+        for (size_t j = 0; j < 4; j++) {
+          const size_t codebook_index = lower_indices[j] + (i + j) * (1 << 4);
+          auto [lower_bound, scale]   = codebook_[codebook_index];
+          decoded_values[j]           = lower_bound + scale * static_cast<float>(upper_values[j]);
+        }
+
+        decoded_vector = vld1q_f32(decoded_values);
+      }
+
+      float32x4_t diff_vector = vsubq_f32(data_vector, decoded_vector);
+      sum_vector              = vmlaq_f32(sum_vector, diff_vector, diff_vector);
+    }
+
+    float32x2_t sum_low    = vget_low_f32(sum_vector);
+    float32x2_t sum_high   = vget_high_f32(sum_vector);
+    float32x2_t sum_half   = vadd_f32(sum_low, sum_high);
+    float32x2_t sum_paired = vpadd_f32(sum_half, sum_half);
+    distance               = vget_lane_f32(sum_paired, 0);
 
     for (size_t i = simd_end; i < dimension_; i++) {
       uint8_t encoded_value     = encoded_codes_[data_index + i];
