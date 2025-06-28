e6c27116736f0defe6a1554bf25c563f92f0a1ee
HuXin0817 <202219120810@stu.cdut.edu.cn>
2025-06-28 14:50:01 +0800

    feat(python): Add Python binding support and build scripts
    Add Python binding implementation, including binding.cpp, setup.py and build.sh
    Add Python bindings build options in CMakeLists.txt
    Python interface to implement PLSQ and SQQuantizer

---
+++ python/binding.cpp
@@ -0,0 +1,60 @@
+#include "../libplsq/ivf.hpp"
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+#define BIND_Quantizer(name, type)                                                                                     \
+  py::class_<type>(m, name)                                                                                            \
+      .def(py::init<size_t, size_t, size_t>(), "Constructor", py::arg("c_bit"), py::arg("q_bit"), py::arg("sub"))      \
+      .def(                                                                                                            \
+          "train",                                                                                                     \
+          [](type &self, const py::array_t<float> &data) { self.train(data.data(), data.size()); },                    \
+          py::arg("data"))                                                                                             \
+      .def("__getitem__", &type::operator[], py::arg("index"))                                                         \
+      .def("size", &type::size)                                                                                        \
+      .def(                                                                                                            \
+          "l2_distance",                                                                                               \
+          [](type &self, const py::array_t<float> &data, size_t idx) {                                                 \
+            float  dis    = 0.0f;                                                                                      \
+            size_t offset = idx * data.size();                                                                         \
+            for (size_t i = 0; i < data.size(); i++) {                                                                 \
+              float dif = data.data()[i] - self[offset + i];                                                           \
+              dis += dif * dif;                                                                                        \
+            }                                                                                                          \
+            return dis;                                                                                                \
+          },                                                                                                           \
+          py::arg("data"),                                                                                             \
+          py::arg("i"))
+
+PYBIND11_MODULE(plsq, m) {
+  m.doc() = R"pbdoc(Piecewise-Optimized Uniform Quantization (POUQ))pbdoc";
+
+  using SQQuantizer = plsq::QuantizerImpl<plsq::Clusterer, plsq::MinMaxOptimizer>;
+  BIND_Quantizer("SQQuantizer", SQQuantizer);
+  BIND_Quantizer("PLSQQuantizer", plsq::PLSQQuantizer);
+
+  py::class_<IvfIndex>(m, "IvfIndex")
+      .def(py::init<size_t, size_t>(), py::arg("nlist"), py::arg("dim"))
+      .def(
+          "train",
+          [](IvfIndex &self, const py::array_t<float> &data) { self.train(data.data(), data.size()); },
+          py::arg("data"))
+      .def(
+          "search",
+          [](IvfIndex &self, const py::array_t<float> &query, size_t k, size_t nprobe) {
+            return self.search(query.data(), k, nprobe);
+          },
+          py::arg("query"),
+          py::arg("k"),
+          py::arg("nprobe"));
+
+  m.def("compute_mse", [](const SQQuantizer &sq, const py::array_t<float> &data) {
+    return l2distance(sq, data.data(), data.size()) / static_cast<float>(data.size());
+  });
+
+  m.def("compute_mse", [](const plsq::PLSQQuantizer &sq, const py::array_t<float> &data) {
+    return l2distance(sq, data.data(), data.size()) / static_cast<float>(data.size());
+  });
+}
