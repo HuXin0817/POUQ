80536beffce0cda8aee7cbc3b0090f9d76ee9e3b
HuXin0817 <202219120810@stu.cdut.edu.cn>
2025-12-06 15:31:37 +0800

    add decode function for distance calculations with SIMD optimization

--- libpouq/distance.h
+++ libpouq/distance.h
@@ -6,3 +6,6 @@
 
 float
 distance(int dim, const CodeUnit* code, const RecPara* rec_para, const float* data, int offset);
+
+void
+decode(int dim, const CodeUnit* code, const RecPara* rec_para, float* dist, int offset);
