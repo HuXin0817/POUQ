4f4f9cd6564c106f2c2417bd2455a2db08a29ed1
HuXin0817 <202219120810@stu.cdut.edu.cn>
2026-05-02 16:28:06 +0800

    build: specify target pypouq in build command
    The build command now explicitly builds the pypouq target instead of all targets by default

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 git submodule update --init --recursive
 cmake -B build -S . -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=${1:-Release}
 ln -sf build/compile_commands.json compile_commands.json
-cmake --build build -j $(nproc)
+cmake --build build -j $(nproc) --target pypouq
